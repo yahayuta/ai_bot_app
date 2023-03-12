5b2427ddfe800495e4fa7346f2a07a5a40a7aa90
やはゆた <13937582+yahayuta@users.noreply.github.com>
2023-03-13 08:47:29 +0900

    initiate for line bot app

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def openai_gpt():
     print(ai_response)
 
     return ai_response
-    
+
+@app.route("/openai_gpt_line", methods=["POST"])
+def openai_gpt_line():
+    return "hoge"
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
